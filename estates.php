920a6ef6bb2a5414391caa7da707d8a9fe708376
Blendi <blendidedushi@gmail.com>
2023-10-13 00:29:23 +0200

    Estate cards

--- estates.php
+++ estates.php
@@ -1,2 +1,19 @@
 <?php include 'includes/header.php' ?>
+<div class="container sortFilterSearch">
+
+</div>
+<div class="container row">
+    <div class="card m-3">
+    <img src="assets/images/estate-card.png" class="card-img-top" alt="estate-card">
+    <div class="card-body">
+        <h5 class="card-title">estate.name</h5>
+        <p class="card-text">estate.location | estate.price&euro;</p>
+        <div class="d-flex justify-content-between align-items-center">
+            <p class="card-text">Review</p>
+            <a href="#" class="btn btn-sm btn-outline-dark"><i class="bi bi-box-arrow-in-right"></i></a>
+        </div>
+    </div>
+    </div>
+</div>
+
 <?php include 'includes/footer.php' ?>
