047a71a378268ff901b146d9c35e7c6b2a59fcc6
Blendi <blendidedushi@gmail.com>
2023-10-12 17:11:24 +0200

    Header | Footer

---
+++ includes/footer.php
@@ -0,0 +1,8 @@
+<footer class="footer bg-secondary bg-light border">
+    <div class="container d-flex justify-content-center">
+        <p class="mt-2">Copyright &copy; SuiteDreams, <?= date('Y') ?></p>
+    </div>
+</footer>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>   
+</body>
+</html>
---
+++ includes/header.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SuiteDreams</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+</head>
+<body>
+<header>
+<nav class="navbar navbar-expand-lg bg-light border">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">SuiteDreams</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav ms-auto">
+        <div class="navbar-nav mx-5">
+          <a class="nav-link" aria-current="page" href="index.php">Home</a>
+          <a class="nav-link" href="estates.php">Estates</a>
+          <a class="nav-link" href="profile.php">MyProfile</a>
+        </div>
+
+        <div class="navbar-nav">
+          <a class="nav-link" href="register.php">SignUp</a>
+          <a class="nav-link" href="login.php">LogIn</a>
+          <a class="nav-link" href="?action=logout">LogOut</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</nav>
+</header>
